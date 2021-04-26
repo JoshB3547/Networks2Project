417d88aa696ce055240e94450391b0f86ece0f48
Bento-Joshua-unilu <79990202+JoshB3547@users.noreply.github.com>
2021-04-26 17:05:50 +0200

    Added base project

---
+++ src/Client.java
@@ -0,0 +1,28 @@
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//setup Nodes and Client
+		
+		//connect to specific Node
+
+		//WHILE scanner running: checks whether to send a query or to "STOP" 
+			//1. send Query and get answer
+			//2. if STOP then set running to false
+		
+	}
+
+	public void connectToNode(String port) {
+		
+	}
+	
+	public void sendSetQuery(int id, String key, String value) {
+		
+	}
+	
+	public void sendGetQuery(int id, String key) {
+		
+	}
+}
---
+++ src/Node.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Node {
+
+	private Map<String, String> keyValueMap;
+	private ArrayList<Node> nodeList;
+	
+	public void connectToNode() {
+		
+	}
+	
+	public void executeSetQuery() {
+		
+	}
+	
+	public void executeGetQuery() {
+		//checkCurrentNodeForKey
+		
+		//connect to another node and check int that node
+			//connect to other nodes or setup broadcast
+	}
+	
+	
+	
+}
