8e96d208004ad344c7f06bff4ae827aa098c0450
JoshB3547 <bentoj058@gmail.com>
2021-04-27 18:21:15 +0200

    idk

--- src/Client.java
+++ src/Client.java
@@ -1,20 +1,79 @@
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Scanner;
 public class Client {
+	  /* The server port to which 
+	  the client socket is going to connect */
+	  public final static int SERVICE_PORT = 50001;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  public String serverIPAddress;
+	  public static int serverPort = 0;
+	  
+	 public static void main(String[] args) throws IOException{
+		 Client client = new Client();
+		    try{
+		      /* Instantiate client socket. 
+		      No need to bind to a specific port */
+		      DatagramSocket clientSocket = new DatagramSocket();
+		      
+		      // Get the IP address of the node from the user
+		      InetAddress IPAddress = InetAddress.getByName("localhost");
+		      
+		      // Creating corresponding buffers
+		      byte[] sendingDataBuffer = new byte[1024];
+		      byte[] receivingDataBuffer = new byte[1024];
+		      
+		      /* Converting data to bytes and 
+		      storing them in the sending buffer */
+			  Scanner scanner = new Scanner(System.in);
+			  String queryText = "Very wrong example text indeed";
+			  String connectionText = "234.675.218.451:4000000 ";
 
-		//setup Nodes and Client
-		
-		//connect to specific Node
+			
+			  do{
 
-		//WHILE scanner running: checks whether to send a query or to "STOP" 
-			//1. send Query and get answer
-			//2. if STOP then set running to false
-		
-	}
+				System.out.println("Pls Enter a IP address and PortNumber for the server, like:\nA. <IPAddress>:<PortNumber>. Example: “123.456.678:9999");	 
+				connectionText = scanner.nextLine();
 
-	public void connectToNode(String port) {
+			  } while(client.checkConnection(connectionText));
+
+			  do{
+
+ 				System.out.println("Pls Enter a Query like:\nA. GET:<id>:<key>. Example: “GET:3487563:mykey”.\nor B. SET:<id>:<key>:<value>. Example: “SET:4564561:mykey:myvalue”.");	 
+			  	queryText = scanner.nextLine();
+
+			  } while(client.checkQuery(queryText));
+			 
+		      
+		      sendingDataBuffer = queryText.getBytes();
+		      
+		      // Creating a packet 
+		      DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer,sendingDataBuffer.length,IPAddress, serverPort);
+		      
+		      // sending packet to the server
+		      clientSocket.send(sendingPacket);
+		      
+		      // Get the server response .i.e. capitalized sentence
+		      DatagramPacket receivingPacket = new DatagramPacket(receivingDataBuffer,receivingDataBuffer.length);
+		      clientSocket.receive(receivingPacket);
+		      
+		      // Printing the received data
+		      String receivedData = new String(receivingPacket.getData());
+		      System.out.println("Sent from the server: "+receivedData);
+		      
+		      // Closing the socket connection with the server
+		      clientSocket.close();
+		    }
+		    catch(SocketException e) {
+		      e.printStackTrace();
+		    }
+		  }
+
+	public void connectToNode() {
 		
 	}
 	
@@ -24,5 +83,44 @@ public class Client {
 	
 	public void sendGetQuery(int id, String key) {
 		
+	}
+
+	public boolean checkQuery(String queryText){
+		String[] queryParts = queryText.split(":");
+		boolean flag = true;
+
+		switch (queryParts[0]) {
+			case "GET":
+				if(queryParts.length==3){
+					flag = false;
+				}
+				break;
+		
+			case "SET":
+				if(queryParts.length==4){
+					flag = false;
+				}
+				break;
+		
+			default:
+			System.out.println("Not a real Query. Try again.");
+				break;
+		}
+
+		return flag;
+	}
+	
+	public boolean checkConnection(String connectionText){
+		String[] ipAndPort = connectionText.split(":");
+		boolean flag = true;
+		if(ipAndPort.length==2){
+			serverIPAddress = ipAndPort[0];
+			serverPort = Integer.parseInt(ipAndPort[1]);
+			flag = false;
+		}
+		else{System.out.println("Wrong IPAddress or PortNumber for the server");}
+		
+
+		return flag;
 	}
 }
--- src/Node.java
+++ src/Node.java
@@ -1,26 +1,160 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Node {
-
-	private Map<String, String> keyValueMap;
+	private String ipAddress;
+	private String portNumber;
+	public final static int SERVICE_PORT=50001;
+	
+	private Map<String, String> keyValueMap = new HashMap<String, String>();
 	private ArrayList<Node> nodeList;
 	
+	
+	public Node() {
+	}
+
+	public static void main(String[] args) throws IOException{
+		Node node= new Node();
+		//TEST
+		node.addKeyValue("1", "69");
+		System.out.println(node.keyValueMap.get("1"));
+
+
+
+
+
+
+	    try{
+	      // Instantiate a new DatagramSocket to receive responses from the client
+	      DatagramSocket serverSocket = new DatagramSocket(SERVICE_PORT);
+	      
+	      /* Create buffers to hold sending and receiving data.
+	      It temporarily stores data in case of communication delays */
+	      byte[] receivingDataBuffer = new byte[1024];
+	      byte[] sendingDataBuffer = new byte[1024];
+	      
+	      /* Instantiate a UDP packet to store the 
+	      client data using the buffer for receiving data*/
+	      DatagramPacket inputPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
+	      System.out.println("Waiting for a client to connect...");
+	      
+	      // Receive data from the client and store in inputPacket
+	      serverSocket.receive(inputPacket);
+	      
+	      // Printing out the client sent data
+	      String receivedData = new String(inputPacket.getData());
+	      System.out.println("Sent from the client: "+receivedData);
+
+
+	      
+		  //work on receivedString
+		  sendingDataBuffer = node.checkReceivedQuery(receivedData).toUpperCase().getBytes();
+
+
+	      /* 
+	      * Convert client sent data string to upper case,
+	      * Convert it to bytes
+	      *  and store it in the corresponding buffer. */
+	      //sendingDataBuffer = receivedData.toUpperCase().getBytes();
+	      
+	      // Obtain client's IP address and the port
+	      InetAddress senderAddress = inputPacket.getAddress();
+	      int senderPort = inputPacket.getPort();
+	      
+	      // Create new UDP packet with data to send to the client
+	      DatagramPacket outputPacket = new DatagramPacket(
+	        sendingDataBuffer, sendingDataBuffer.length,
+	        senderAddress,senderPort
+	      );
+	      
+	      // Send the created packet to client
+	      serverSocket.send(outputPacket);
+	      // Close the socket connection
+	      serverSocket.close();
+	    }
+	    catch (SocketException e){
+	      e.printStackTrace();
+	    }
+	  }
+	
+	public Node(String ipAddress, String portNumber) {
+		super();
+		this.ipAddress = ipAddress;
+		this.portNumber = portNumber;
+	}
+
+	
+	public Map<String, String> getKeyValueMap() {
+		return keyValueMap;
+	}
+
+	public void addKeyValue(String key, String value) {
+		getKeyValueMap().put(key, value);
+	}
+
 	public void connectToNode() {
 		
 	}
 	
-	public void executeSetQuery() {
-		
+	public String executeSetQuery(String key, String value) {	
+		String result =  " Key " + key + " and value "+value + " set." ;
+		addKeyValue(key, value);
+		System.out.println("ExecutedSetQuery");
+		return result;
 	}
 	
-	public void executeGetQuery() {
+	public String executeGetQuery(String key) {
 		//checkCurrentNodeForKey
+		String result = "nothin in it";
+		if(getKeyValueMap().containsKey(key)){
+			result = ( " Key " + key+ " holds value " + getKeyValueMap().get(key));
+			System.out.println("ExecutedGetQuery");
+		}
+		else{
+			System.out.println("Keyvalue not inside");
+		}
 		
 		//connect to another node and check int that node
 			//connect to other nodes or setup broadcast
+
+		return result;
 	}
-	
-	
+
+	public String checkReceivedQuery(String queryText){
+		String[] queryParts = queryText.split(":");
+		String queryType = queryParts[0];
+		String returnMessageText = queryParts[0]+" msg w/ id "+queryParts[1]+" rcvd. " ;
+
+		switch (queryType) {
+			case "GET":
+				if(queryParts.length==3){
+					System.out.println("Server needs to execute GET with id:"+queryParts[1]);
+					returnMessageText+=executeGetQuery(queryParts[2]);
+				}
+				break;
+		
+			case "SET":
+				if(queryParts.length==4){
+					System.out.println("Server needs to execute SET with id:"+queryParts[1]);
+					returnMessageText+=executeSetQuery(queryParts[2], queryParts[3]);
+				}
+				break;
+		
+			default:
+				System.out.println("Not a real Query. Try again.");
+				returnMessageText+="didn't work";
+				break;
+		}
+		return returnMessageText;
+	}
+
+
 	
 }
